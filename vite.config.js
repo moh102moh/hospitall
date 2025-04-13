b062293620834072fb6159ffec82efc820f58c8c
moh102moh <ahmad122sj@gmail.com>
2025-04-13 21:20:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
     ],
+    base: '/', // مهم جدًا على الاستضافة عشان المسارات ما تخرب
 });
